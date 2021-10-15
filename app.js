6938a71ce48f18a10df5577597a24f18381b9316
TaniEi <netatani@gmail.com>
2021-10-15 19:21:49 +0200

    init

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors())
+app.use(express.json()); // to accept json data
+
+
+//Routes
+app.get('/', (req, res) => {
+	res.send('Welcome to HomePage')
+});
+
+// Connect MongoDB
+mongoose.connect("mongodb+srv://taniEis:qFjyKWTNi8ww2q8r@cluster0.bqrsw.mongodb.net/testing?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true  }, () => {
+	console.log("Connected to DB")
+});
+
+app.listen(3000, () => {
+	console.log("Running RESTful API on port http://localhost:3000");
+});
